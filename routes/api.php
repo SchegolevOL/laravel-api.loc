c70974d46d2ad817c64f68afeea6c59dbe252c45
Oleg Schegol <olschegolev@yahoo.com>
2024-05-10 16:58:25 +0500

    register

--- routes/api.php
+++ routes/api.php
@@ -13,13 +13,19 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::prefix('v1')->middleware(['throttle:api'])->group(function (){
+    Route::post('register', [\App\Http\Controllers\Api\V1\AuthController::class, 'register']);
+    Route::post('login', [\App\Http\Controllers\Api\V1\AuthController::class, 'login']);
+});
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-Route::prefix('v1')->middleware('throttle:api')->group(function (){
+Route::prefix('v1')->middleware(['throttle:api', 'auth:sanctum'])->group(function (){
     Route::apiResource('category', \App\Http\Controllers\Api\V1\CategoryController::class);
     Route::apiResource('posts', \App\Http\Controllers\Api\V1\PostController::class);
+    Route::get('logout',[\App\Http\Controllers\Api\V1\AuthController::class, 'logout']);
 });
 
 
